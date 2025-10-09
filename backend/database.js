83eff381cccc619961477073f50fdfd03165af41
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-10-09 06:16:43 -0300

    computer changing

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,16 @@
 import sqlite3 from "sqlite3";
 const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./domus.db",
+});
 
-const db = new sqlite3.Database("domus.db");
+const Familia = Database.define("familia", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  pessoa_referencia: {},
+});
