177f8fa3b3ea25421ad7144587fcc2fd5707aa1a
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2026-01-15 16:55:13 -0300

    DB MODEL
    Three main entities has been created. families, people and events. basic database will be created next

--- backend/test.sql
+++ backend/test.sql
@@ -3,14 +3,28 @@ CREATE TABLE especificidades_tipo(
     tipo_especificade VARCHAR(100)
 );
 
+CREATE TABLE tipo_eventos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100)
+);
+
+CREATE TABLE eventos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100),
+    data_evento DATETIME,
+    endereco VARCHAR(100),
+    tipo_evento INTEGER NOT NULL,
+    FOREIGN KEY (tipo_evento) REFERENCES tipo_eventos(id)
+
+);
+
+
 CREATE TABLE familias (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(100),
     endereco varchar(100),
     especificidade INTEGER NULL,
-    FOREIGN KEY (especificidade) REFERENCES especificidades_tipo(id),
-    responsavel INTEGER NULL,
-    FOREIGN KEY (responsavel) REFERENCES pessoas(id)
+    FOREIGN KEY (especificidade) REFERENCES especificidades_tipo(id)
 );
 
 CREATE TABLE pessoas (
@@ -19,22 +33,38 @@ CREATE TABLE pessoas (
     data_nascimento DATE,
     contato VARCHAR(11),
     familia INTEGER NOT NULL,
-    FOREIGN KEY (familia) REFERENCES familias(id)
+    FOREIGN KEY (familia) REFERENCES familias(id),
+    eh_responsavel BOOLEAN NOT NULL
 );
 
-INSERT into especificidades_tipo (tipo_especificade) VALUES ('Recém-Casados'),
-('Filhos Especiais'), ('Casos Especiais'), ('Viuvos'), ('Idosos'), ('Sozinhos'), ('Distanciados'), ('Drogas');
-
-INSERT into pessoas (nome, data_nascimento, contato)
-VALUES
-('Victor Emanuel Silva Viana', 03/03/1988, '88997141533'),
-('Bruna Correia da Silva', 15/11/1988, '88997218758');
-
-INSERT into familias (nome, endereco, especificidade, responsavel)
-VALUES
-('Viana', 'Rua Prof. Jose Viana Filho, 70', 01)
 
 
-SELECT id, nome FROM familias WHERE especificidade = 1;
+CREATE TABLE participacoes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    evento INTEGER NOT NULL,
+    pessoa INTEGER NOT NULL,
+    FOREIGN KEY (evento) REFERENCES eventos(id),
+    FOREIGN KEY (pessoa) REFERENCES pessoas(id)
+);
 
-SELECT * FROM familias WHERE nome = 'Viana';
+
+
+
+
+
+-- INSERT into especificidades_tipo (tipo_especificade) VALUES ('Recém-Casados'),
+-- ('Filhos Especiais'), ('Casos Especiais'), ('Viuvos'), ('Idosos'), ('Sozinhos'), ('Distanciados'), ('Drogas');
+
+-- INSERT into pessoas (nome, data_nascimento, contato)
+-- VALUES
+-- ('Victor Emanuel Silva Viana', 03/03/1988, '88997141533'),
+-- ('Bruna Correia da Silva', 15/11/1988, '88997218758');
+
+-- INSERT into familias (nome, endereco, especificidade, responsavel)
+-- VALUES
+-- ('Viana', 'Rua Prof. Jose Viana Filho, 70', 01)
+
+
+-- SELECT id, nome FROM familias WHERE especificidade = 1;
+
+-- SELECT * FROM familias WHERE nome = 'Viana';
