67f743222498ef76df29a3e94b896de59c055b83
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2026-04-03 16:40:31 -0300

    Family register started

--- frontend/src/components/AdicionarFamilia.jsx
+++ frontend/src/components/AdicionarFamilia.jsx
@@ -1,3 +1,34 @@
+import { useState, useEffect } from "react";
+
 export function AdicionarFamilia() {
-  return <div>Essa tela vai adicionar familias</div>;
+  const [especificidadeId, setEspecificidadeId] = useState([]);
+  const [especificidades, setEspecificidades] = useState([]);
+  useEffect(() => {
+    getEspecificidade();
+  }, []);
+
+  function getEspecificidade() {
+    fetch("http://localhost:3000/especificidades")
+      .then((res) => res.json())
+      .then((data) => setEspecificidades(data));
+  }
+  function adicionarFamilia(formData) {}
+  return (
+    <div>
+      <form action={adicionarFamilia}>
+        <input name="nome"></input>
+        <input name="endereco"></input>
+        <select
+          value={especificidadeId}
+          onChange={(e) => setEspecificidadeId(e.target.value)}
+        >
+          {especificidades.map((especificidade) => {
+            return (
+              <option value={especificidade.id}>{especificidade.tipo}</option>
+            );
+          })}
+        </select>
+      </form>
+    </div>
+  );
 }
