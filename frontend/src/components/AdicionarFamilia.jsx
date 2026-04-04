86a28634c359e1d69e2e31e69bf4e2b6fd21ce02
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2026-04-04 16:51:02 -0300

    COMPUTER CHANGE
    ONGOING CHANGES using FormData to send to backend via POST data to
    store family

--- frontend/src/components/AdicionarFamilia.jsx
+++ frontend/src/components/AdicionarFamilia.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
 
 export function AdicionarFamilia() {
   const [especificidadeId, setEspecificidadeId] = useState([]);
   const [especificidades, setEspecificidades] = useState([]);
+
   useEffect(() => {
     getEspecificidade();
   }, []);
@@ -12,23 +14,56 @@ export function AdicionarFamilia() {
       .then((res) => res.json())
       .then((data) => setEspecificidades(data));
   }
-  function adicionarFamilia(formData) {}
+  function adicionarFamilia(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const data = Object.fromEntries(formData.entries());
+    console.log(JSON.stringify(data));
+
+    fetch("http://localhost:3000/familia", {
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((res) => console.log(res));
+  }
   return (
-    <div>
-      <form action={adicionarFamilia}>
-        <input name="nome"></input>
-        <input name="endereco"></input>
-        <select
-          value={especificidadeId}
-          onChange={(e) => setEspecificidadeId(e.target.value)}
-        >
-          {especificidades.map((especificidade) => {
-            return (
-              <option value={especificidade.id}>{especificidade.tipo}</option>
-            );
-          })}
-        </select>
-      </form>
-    </div>
+    <Form onSubmit={adicionarFamilia}>
+      <Form.Group controld="formAdicionarFamilia">
+        <Form.Label>Nome da Familia</Form.Label>
+        <Form.Control
+          name="nome"
+          type="text"
+          placeholder="Insira o nome da Família"
+          required
+        />
+        <Form.Label>Endereço</Form.Label>
+        <Form.Control
+          name="endereco"
+          type="text"
+          placeholder="Insira o endereço da familia"
+          required
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Cadastrar
+      </Button>
+    </Form>
+    // <div>
+    //   <form action={adicionarFamilia}>
+    //     <input name="nome"></input>
+    //     <input name="endereco"></input>
+    //     <select
+    //       value={especificidadeId}
+    //       onChange={(e) => setEspecificidadeId(e.target.value)}
+    //     >
+    //       {especificidades.map((especificidade) => {
+    //         return (
+    //           <option value={especificidade.id}>{especificidade.tipo}</option>
+    //         );
+    //       })}
+    //     </select>
+    //   </form>
+    // </div>
   );
 }
